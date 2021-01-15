522f2e5ad27b1f551a7554aa64921dbd65347ea8
Daniel Nuttall <danielrogernuttall@gmail.com>
2021-01-15 23:37:24 +0200

    Implemented all functionality
    Added delete and add functionality.
    Need to fix:
    1) Item isn't added when clicking submit, need to fix that.

--- angular-to-do-list-app/src/app/components/todo-item/todo-item.component.ts
+++ angular-to-do-list-app/src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TodoService } from '../../services/todo.service'
+
 import { Todo } from '../../models/Todo'
 
 @Component({
@@ -8,8 +10,9 @@ import { Todo } from '../../models/Todo'
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit() {
   }
@@ -23,5 +26,15 @@ export class TodoItemComponent implements OnInit {
 
     return classes;
   }
+
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
+  }
+  onDelete(todo) {
+    this.deleteTodo.emit(todo)
+  }
+  
+
 
 }
--- angular-to-do-list-app/src/app/components/todos/todos.component.ts
+++ angular-to-do-list-app/src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../models/Todo'
+import { TodoService } from '../../services/todo.service'
 
 @Component({
   selector: 'app-todos',
@@ -10,26 +11,24 @@ export class TodosComponent implements OnInit {
 
   todos:Todo[];
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit() {
-    this.todos = [
-      {
-        id: 1,
-        title: "Todo One",
-        completed: false
-      },
-      {
-        id: 2,
-        title: "Todo Two",
-        completed: false
-      },
-      {
-        id: 3,
-        title: "Todo Three",
-        completed: true
-      }
-    ]
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
+  }
+
+  deleteTodo(todo:Todo)
+  {
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo:Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo)
+    })
   }
 
 }
